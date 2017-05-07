29dde48692e36b4ea57435b76083f35d2b7c6535
zhukeshuai <feng_xing@126.com>
2017-05-07 12:40:50 +0800

    去掉文件跟踪

--- Pods/Target Support Files/Pods-QDBorrow-QDBorrowTests/Pods-QDBorrow-QDBorrowTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_QDBorrow_QDBorrowTests : NSObject
-@end
-@implementation PodsDummy_Pods_QDBorrow_QDBorrowTests
-@end
--- Pods/Target Support Files/Pods-QDBorrow/Pods-QDBorrow-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_QDBorrow : NSObject
-@end
-@implementation PodsDummy_Pods_QDBorrow
-@end
