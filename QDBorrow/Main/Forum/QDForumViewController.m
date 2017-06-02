9e519e95416a5f15ef684dfb4f87172e2ee143b8
zhukeshuai <feng_xing@126.com>
2017-06-02 23:21:53 +0800

    更新代码

--- QDBorrow/Main/Forum/QDForumViewController.m
+++ QDBorrow/Main/Forum/QDForumViewController.m
@@ -10,8 +10,11 @@
 #import "QMUIKit.h"
 #import "UIColor+QMUI.h"
 #import "MBProgressHUD+MP.h"
+#import "AVQuery.h"
+#import "UIColor+Hex.h"
+#import "QMUIKit.h"
 
-@interface QDForumViewController ()
+@interface QDForumViewController () <UIWebViewDelegate>
 @property (nonatomic, strong) UIWebView *webView;
 @property (nonatomic, strong) NSString *urlStr;
 
@@ -22,12 +25,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    self.webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, -40, SCREEN_WIDTH, SCREEN_HEIGHT + 40)];
+    self.webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)];
+    self.webView.scrollView.bounces = NO;
     [self.view addSubview:self.webView];
+    self.webView.delegate = self;
     self.title = @"论坛";
     [self fetchData];
 }
 
+- (void)viewDidAppear:(BOOL)animated {
+    [self setStatusBarBackgroundColor:UIColorMake(36, 121, 232)];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [self setStatusBarBackgroundColor:[UIColor clearColor]];
+}
 //设置状态栏颜色
 - (void)setStatusBarBackgroundColor:(UIColor *)color {
     
@@ -40,15 +52,31 @@
 
 - (void)fetchData {
     [MBProgressHUD showMessage:@"加载中..." ToView:self.view];
-    AVQuery *queryBanner = [AVQuery queryWithClassName:@"QDBanner"];
-    [queryBanner findObjectsInBackgroundWithBlock:^(NSArray * _Nullable objects, NSError * _Nullable error) {
-    
-    }
-    
-//    [self loadURL:[NSURL URLWithString:@"http://114.215.210.61:10080/bbs/portal.php?mobile=2"]];
+    AVQuery *queryBanner = [AVQuery queryWithClassName:@"QDForum"];
+    [queryBanner getFirstObjectInBackgroundWithBlock:^(AVObject * _Nullable object, NSError * _Nullable error) {
+        [MBProgressHUD hideHUDForView:self.view];
+        self.urlStr = [object objectForKey:@"forumurl"];
+        [self loadWebView];
+    }];
     
 }
 
+- (void)loadWebView {
+    NSURL* url = [NSURL URLWithString:self.urlStr];//创建URL
+    NSURLRequest* request = [NSURLRequest requestWithURL:url];//创建NSURLRequest
+    [self.webView loadRequest:request];//加载
+
+}
+
+
+
+- (void)webViewDidStartLoad:(UIWebView *)webView {
+    [MBProgressHUD showMessage:@"加载中..." ToView:self.view];
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+    [MBProgressHUD hideHUDForView:self.view];
+}
 
 /*
 #pragma mark - Navigation
--- QDBorrow/Resource/Appdelegate/AppDelegate.m
+++ QDBorrow/Resource/Appdelegate/AppDelegate.m
@@ -153,11 +153,11 @@
 //    UIViewController *sobotViewController = [self sobotViewController];
 //    QDNavigationController *certNavController = [[QDNavigationController alloc] initWithRootViewController:sobotViewController];
     
-    QDForumViewController *forumViewController = [[QDForumViewController alloc] initWithAddress:@"http://114.215.210.61:10080/bbs/portal.php?mobile=2"];
+    QDForumViewController *forumViewController = [[QDForumViewController alloc] init];
     QDNavigationController *certNavController = [[QDNavigationController alloc] initWithRootViewController:forumViewController];
     forumViewController.hidesBottomBarWhenPushed = NO;
-//    certNavController.navigationBarHidden = YES;
-    certNavController.tabBarItem = [QDUIHelper tabBarItemWithTitle:@"信用卡" image:[UIImageMake(@"icon_tabbar_lab") imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal] selectedImage:UIImageMake(@"icon_tabbar_lab_selected") tag:2];
+    certNavController.navigationBarHidden = YES;
+    certNavController.tabBarItem = [QDUIHelper tabBarItemWithTitle:@"论坛" image:[UIImageMake(@"icon_tabbar_lab") imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal] selectedImage:UIImageMake(@"icon_tabbar_lab_selected") tag:2];
     
 //    
 //    QDCertViewController *cerditController = [[QDCertViewController alloc] init];
