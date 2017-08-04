cee930a08dbbbb9372c5a33ddb3b6e0546f9a389
larou <larou@2dfire.com>
2017-08-04 15:34:47 +0800

    更新代码

---
+++ QDBorrow/Main/BorrowMain/View/QDHomeBorrowCell.h
@@ -0,0 +1,13 @@
+//
+//  QDHomeBorrowCell.h
+//  QDBorrow
+//
+//  Created by larou on 2017/8/4.
+//  Copyright © 2017年 jinrong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QDHomeBorrowCell : UITableViewCell
+
+@end
