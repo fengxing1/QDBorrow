4808f5983d3322d68889fc2d633e53e8a4c4dc8b
larou <larou@2dfire.com>
2017-09-01 17:17:36 +0800

    添加系统消息

---
+++ QDBorrow/Main/BorrowMain/Controller/QDMessageCell.h
@@ -0,0 +1,13 @@
+//
+//  QDMessageCell.h
+//  QDBorrow
+//
+//  Created by larou on 2017/9/1.
+//  Copyright © 2017年 jinrong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QDMessageCell : UITableViewCell
+
+@end
---
+++ QDBorrow/Main/BorrowMain/Controller/QDMessageCell.m
@@ -0,0 +1,24 @@
+//
+//  QDMessageCell.m
+//  QDBorrow
+//
+//  Created by larou on 2017/9/1.
+//  Copyright © 2017年 jinrong. All rights reserved.
+//
+
+#import "QDMessageCell.h"
+
+@implementation QDMessageCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
