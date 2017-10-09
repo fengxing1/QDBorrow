b9a23b93c55d729f535704df283c6e8496fc3ebe
zhukeshuai <feng_xing@126.com>
2017-10-09 22:17:50 +0800

    更新代码

--- QDBorrow/Main/Ohters/Controller/QDWebViewController.m
+++ QDBorrow/Main/Ohters/Controller/QDWebViewController.m
@@ -7,16 +7,26 @@
 //
 
 #import "QDWebViewController.h"
+#import "QMUICommonDefines.h"
 
 @implementation QDWebViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.webView.translatesAutoresizingMaskIntoConstraints = YES;
+    self.webView.frame = CGRectMake(0,0, SCREEN_WIDTH, SCREEN_HEIGHT);
+    if (!self.isLuntan) {
+        self.webView.frame = CGRectMake(0,0, SCREEN_WIDTH, SCREEN_HEIGHT-64);
+    }
+    self.webView.scrollView.bounces = YES;
 }
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     [self.navigationController setNavigationBarHidden:NO animated:NO];
+
 }
+
+
 
 @end
