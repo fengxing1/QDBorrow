3dbbe6191e86808a49bdcbd7e4a19654eace2d31
zhukeshuai <feng_xing@126.com>
2017-11-02 22:46:08 +0800

    更新代码

---
+++ QDBorrow/Main/SheBao/My/MyViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyViewController.h
+//  QDBorrow
+//
+//  Created by 朱恪帅 on 2017/10/30.
+//  Copyright © 2017年 jinrong. All rights reserved.
+//
+
+#import <QMUIKit/QMUIKit.h>
+
+@interface MyViewController : QMUICommonTableViewController
+
+@end
---
+++ QDBorrow/Main/SheBao/Tools/ToolsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ToolsViewController.h
+//  QDBorrow
+//
+//  Created by 朱恪帅 on 2017/10/30.
+//  Copyright © 2017年 jinrong. All rights reserved.
+//
+
+#import <QMUIKit/QMUIKit.h>
+
+@interface ToolsViewController : QMUICommonTableViewController
+
+@end
